6fbfc9ade31c675c6483f1e88f138886a1f7694b
Alvaro <alvarogy2004@gmail.com>
2024-04-23 22:31:41 +0200

    controlador api terminado

--- Api-Ejercicios/src/main/java/com/example/apiejercicios/Controller/ControllerEjercicio.java
+++ Api-Ejercicios/src/main/java/com/example/apiejercicios/Controller/ControllerEjercicio.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -15,69 +16,78 @@ public class ControllerEjercicio {
 	@Autowired
 	private RepositoryEjercicio ejerciciosRepository;
 
-	@GetMapping("/ejercicios")
-	public ResponseEntity<Iterable<EjercicioORM>> getEjercicios() {
-		return ResponseEntity.ok(ejerciciosRepository.findAll());
+	@GetMapping("/ejercicios/buscar")
+	public ResponseEntity<Iterable<EjercicioORM>> buscarEjercicios(@RequestParam(required = false) String nombre,
+			@RequestParam(required = false) String musculo, @RequestParam(required = false) String equipamiento) {
+
+		if (nombre != null) {
+			if (musculo != null && equipamiento != null) {
+				return findByNombreAndEquipamientoAndMusculo(nombre, equipamiento, musculo);
+			} else if (musculo != null) {
+				return findByNombreAndMusculo(nombre, musculo);
+			} else if (equipamiento != null) {
+				return findByNombreAndEquipamiento(nombre, equipamiento);
+			} else {
+				return findByNombre(nombre);
+			}
+		} else if (musculo != null) {
+			if (equipamiento != null) {
+				return findByMusculoAndEquipamiento(musculo, equipamiento);
+			} else {
+				return findByMusculo(musculo);
+			}
+		} else if (equipamiento != null) {
+			return findByEquipamiento(equipamiento);
+		} else {
+			return null;
+		}
 	}
 
 	@GetMapping("/ejercicios/nombre")
-	public ResponseEntity<Iterable<EjercicioORM>> getEjerciciosByNombre(@RequestParam String nombre) {
+	public ResponseEntity<Iterable<EjercicioORM>> findByNombre(@RequestParam(required = false) String nombre) {
 		return ResponseEntity.ok(ejerciciosRepository.findByNombreContaining(nombre));
 	}
 
-	@GetMapping("/ejercicios/musculo")
-	public ResponseEntity<Iterable<EjercicioORM>> getEjerciciosByMusculo(@RequestParam String musculo) {
+	@GetMapping("/ejercicios/buscarPorMusculo")
+	public ResponseEntity<Iterable<EjercicioORM>> findByMusculo(@RequestParam(required = false) String musculo) {
 		return ResponseEntity.ok(ejerciciosRepository.findByMusculoContaining(musculo));
+
 	}
 
-	@GetMapping("/ejercicios/equipamiento")
-	public ResponseEntity<Iterable<EjercicioORM>> getEjerciciosByEquipamieto(@RequestParam String equipamiento) {
+	@GetMapping("/ejercicios/buscarPorEquipamiento")
+	public ResponseEntity<Iterable<EjercicioORM>> findByEquipamiento(
+			@RequestParam(required = false) String equipamiento) {
 		return ResponseEntity.ok(ejerciciosRepository.findByEquipamientoContaining(equipamiento));
 	}
 
-	@GetMapping("/ejercicios/{id}")
-	public ResponseEntity<EjercicioORM> getEjercicio(@PathVariable(name = "id") Long id) {
-		Optional<EjercicioORM> ejercicioOptional = ejerciciosRepository.findById(id);
-		return ejercicioOptional.map(ResponseEntity::ok)
-				.orElse(ResponseEntity.notFound().build());
+	@GetMapping("/ejercicios/buscarPorMusculoYEquipamiento")
+	public ResponseEntity<Iterable<EjercicioORM>> findByMusculoAndEquipamiento(
+			@RequestParam(required = false) String musculo, @RequestParam(required = false) String equipamiento) {
+		return ResponseEntity
+				.ok(ejerciciosRepository.findByMusculoContainingAndEquipamientoContaining(musculo, equipamiento));
 	}
 
-	@PostMapping("/ejercicios")
-	public ResponseEntity<EjercicioORM> createEjercicio(@RequestBody EjercicioORM producto) {
-		return ResponseEntity.status(HttpStatus.CREATED).body(ejerciciosRepository.save(producto));
+	@GetMapping("/ejercicios/buscarPorNombreYMusculo")
+	public ResponseEntity<Iterable<EjercicioORM>> findByNombreAndMusculo(@RequestParam(required = false) String nombre,
+			@RequestParam(required = false) String musculo) {
+		return ResponseEntity.ok(ejerciciosRepository.findByNombreContainingAndMusculoContaining(nombre, musculo));
+
 	}
 
-	@PutMapping("/ejercicios/{id}")
-	public ResponseEntity<EjercicioORM> updateEjercicio(@PathVariable(name = "id") Long id,
-														@RequestBody EjercicioORM ejercicioORM) {
-		return ResponseEntity.ok(ejerciciosRepository.save(ejercicioORM));
+	@GetMapping("/ejercicios/buscarPorNombreYEquipamiento")
+	public ResponseEntity<Iterable<EjercicioORM>> findByNombreAndEquipamiento(
+			@RequestParam(required = false) String nombre, @RequestParam(required = false) String equipamiento) {
+		return ResponseEntity
+				.ok(ejerciciosRepository.findByNombreContainingAndEquipamientoContaining(nombre, equipamiento));
+
 	}
 
-	@DeleteMapping("/ejercicios/{id}")
-	public ResponseEntity<Void> deleteEjercicio(@PathVariable(name = "id") Long id) {
-		ejerciciosRepository.deleteById(id);
-		return ResponseEntity.noContent().build();
+	@GetMapping("/ejercicios/buscarPorNombreYEquipamientoYMusculo")
+	public ResponseEntity<Iterable<EjercicioORM>> findByNombreAndEquipamientoAndMusculo(
+			@RequestParam(required = false) String nombre, @RequestParam(required = false) String equipamiento,
+			@RequestParam(required = false) String musculo) {
+		return ResponseEntity.ok(ejerciciosRepository
+				.findByNombreContainingAndEquipamientoContainingAndMusculoContaining(nombre, equipamiento, musculo));
+
 	}
-
-	/*@PatchMapping("/ejercicios/{id}")
-	public ResponseEntity<Ejercicio> patchEjercicio(@PathVariable(name = "id") Long id,
-			@RequestBody Ejercicio ejercicioPatch) {
-		// Encuentra el ejercicio por ID
-		Ejercicio ejercicio = ejerciciosRepository.findById(id).orElse(null);
-
-		if (ejercicio == null) {
-			return ResponseEntity.notFound().build();
-		}
-
-		// Aplica los cambios parciales del ejercicio recibido al ejercicio existente
-		if (ejercicioPatch.getNombre() != null) {
-			ejercicio.setNombre(ejercicioPatch.getNombre());
-		}
-		if (ejercicioPatch.getDescripcion() != null) {
-			ejercicio.setDescripcion(ejercicioPatch.getDescripcion());
-		}
-
-		// Guarda y retorna el ejercicio actualizado
-		return ResponseEntity.ok(ejerciciosRepository.save(ejercicio));
-	}*/
 }
--- Api-Ejercicios/src/main/java/com/example/apiejercicios/repositorio/RepositoryEjercicio.java
+++ Api-Ejercicios/src/main/java/com/example/apiejercicios/repositorio/RepositoryEjercicio.java
@@ -9,10 +9,31 @@ import org.springframework.data.repository.query.Param;
 
 public interface RepositoryEjercicio extends CrudRepository<EjercicioORM,Long> {
 
-  List<EjercicioORM> findByNombreContaining(@Param("nombre") String nombre);
-  List<EjercicioORM> findByMusculoContaining(@Param("musculo") String musculo);
-  List<EjercicioORM> findByEquipamientoContaining(@Param("equipamiento") String equipamiento);
+  List<EjercicioORM> findByNombreContaining(
+		  @Param("nombre") String nombre);
+  
+  List<EjercicioORM> findByMusculoContaining(
+		  @Param("musculo") String musculo);
+  
+  List<EjercicioORM> findByEquipamientoContaining(
+		  @Param("equipamiento") String equipamiento);
 
+  List<EjercicioORM> findByMusculoContainingAndEquipamientoContaining(
+          @Param("musculo") String musculo,
+          @Param("equipamiento") String equipamiento);
+  
+  List<EjercicioORM> findByNombreContainingAndMusculoContaining(
+		  @Param("nombre") String nombre, 
+		  @Param("musculo") String musculo);
+  
+  List<EjercicioORM> findByNombreContainingAndEquipamientoContaining(
+		  @Param("nombre") String nombre, 
+		  @Param("equipamiento") String equipamiento);
+  
+  List<EjercicioORM> findByNombreContainingAndEquipamientoContainingAndMusculoContaining(
+		  @Param("nombre") String nombre, 
+		  @Param("equipamiento") String equipamiento, 
+		  @Param("musculo") String musculo);	
 
 
 
