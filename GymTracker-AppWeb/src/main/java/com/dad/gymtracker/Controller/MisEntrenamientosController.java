49cc1ecd1b0455f92b1fe30252bad0ac311fbe48
deiivvv <davidmd95@hotmail.com>
2024-05-01 21:02:50 +0200

    pantalla mis entrenamientos

---
+++ GymTracker-AppWeb/src/main/java/com/dad/gymtracker/Controller/MisEntrenamientosController.java
@@ -0,0 +1,26 @@
+package com.dad.gymtracker.Controller;
+
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.dad.gymtracker.Dto.MisEntrenamientosDTO;
+import com.dad.gymtracker.Service.MisEntrenamientosService;
+import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
+
+@Controller
+@AllArgsConstructor
+public class MisEntrenamientosController {
+
+	private final MisEntrenamientosService misEntrenamientosService;
+
+	@GetMapping("/mis-entrenamientos")
+	public String misEntrenamientos(Model model, HttpSession session) {
+		List<MisEntrenamientosDTO> listaEntrenamientos =misEntrenamientosService.buscarEntrenamientosById((int) session.getAttribute("idUsuario"));
+		if(!listaEntrenamientos.isEmpty()) {
+			model.addAttribute("listaEntrenamientos", listaEntrenamientos);
+		}
+		return "misEntrenamientos";
+	}
+}
