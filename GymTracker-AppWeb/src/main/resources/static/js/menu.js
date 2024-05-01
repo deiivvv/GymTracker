466c133009892aac31dc42c1baa08221d46e4fd1
Alvaro <alvarogy2004@gmail.com>
2024-05-01 15:14:54 +0200

    javascript cambiarColres menu

--- GymTracker-AppWeb/src/main/resources/static/js/menu.js
+++ GymTracker-AppWeb/src/main/resources/static/js/menu.js
@@ -1,18 +1,3 @@
-const hombreImages = ['hombre1.png', 'hombre2.png'];
-const mujerImages = ['hombre1.png', 'hombre2.png']; 
-
-let index = 0;
-
-function cambioImages() {
-
-    if(index >= hombreImages.length || index >= mujerImages.length){
-        index = 0;
-    }
-    document.getElementById("idImagenHombre").src= "./images/menu/hombre/" + hombreImages[index];
-    document.getElementById("idImagenMujer").src= "./images/menu/hombre/" + mujerImages[index];
-
-    index++;
-
+function cambioColores(li, color){
+	li.querySelector("a").style="color:" + color;
 }
-
-setInterval(cambioImages, 3000);
