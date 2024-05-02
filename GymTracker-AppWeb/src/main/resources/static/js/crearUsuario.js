895efe4919f4946191d2f7737804f7897db214a0
Alvaro <alvarogy2004@gmail.com>
2024-05-02 02:04:57 +0200

    animacion

--- GymTracker-AppWeb/src/main/resources/static/js/crearUsuario.js
+++ GymTracker-AppWeb/src/main/resources/static/js/crearUsuario.js
@@ -1,14 +1,27 @@
 function siguiente(numPagina) {
     let contenedorActual = document.getElementById('idContenedor' + numPagina);
-    contenedorActual.classList.toggle('oculto');
     let contenedorSiguiente = document.getElementById('idContenedor' + (numPagina + 1));
+    
+    contenedorActual.classList.toggle('oculto');
     contenedorSiguiente.classList.toggle('oculto');
+    
+    contenedorSiguiente.classList.remove('giroVolver');
+    setTimeout(function() {
+    	contenedorSiguiente.classList.add('giroSiguiente');
+    }, 10);
 }
 
 function volver(numPagina) {
     let contenedorActual = document.getElementById('idContenedor' + numPagina);
     let contenedorAnterior = document.getElementById('idContenedor' + (numPagina - 1));
-
-    contenedorActual.classList.toggle('oculto');
-    contenedorAnterior.classList.toggle('oculto');
+	
+	contenedorActual.classList.toggle('oculto');
+	contenedorAnterior.classList.toggle('oculto');
+	
+	contenedorAnterior.classList.remove('giroSiguiente');
+	
+	setTimeout(function() {
+    	contenedorAnterior.classList.add('giroVolver');
+    }, 10);
 }
+ 	
