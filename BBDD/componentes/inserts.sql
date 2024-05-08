6338bf2dd37570eef417c319bd08dd1b65d494fc
AlvaroGalle <120171332+AlvaroGalle@users.noreply.github.com>
2024-05-08 09:38:43 +0200

    Update inserts.sql para que coincidan con la api de ejercicios

--- BBDD/componentes/inserts.sql
+++ BBDD/componentes/inserts.sql
@@ -1,6 +1,4 @@
-
 USE gymtracker;
-
 
 -- Insertar usuario ficticio 'damago' y su perfil
 INSERT INTO usuarios (nombre, contrasena, rol) VALUES ('damago', 'damago', 'usuario');
@@ -12,15 +10,26 @@ INSERT INTO perfil (id_usuario, genero, edad, altura, peso) VALUES (LAST_INSERT_
 INSERT INTO usuarios (nombre, contrasena, rol) VALUES ('david', 'david', 'bloqueado');
 INSERT INTO perfil (id_usuario, genero, edad, altura, peso) VALUES (LAST_INSERT_ID(), 'Masculino', 25, 180, 75);
 
+-- Insertar ejercicio 'sentadillas'
+INSERT INTO ejercicios (nombre) VALUES ('sentadillas');
+-- Obtener el ID del ejercicio 'sentadillas' insertado
+SET @id_sentadillas = LAST_INSERT_ID();
+
 -- Insertar ejercicio 'press banca'
-INSERT INTO ejercicios (nombre) VALUES ('press banca');
+INSERT INTO ejercicios (nombre) VALUES ('press de banca');
 -- Obtener el ID del ejercicio 'press banca' insertado
 SET @id_press_banca = LAST_INSERT_ID();
 
--- Insertar ejercicio 'press militar'
-INSERT INTO ejercicios (nombre) VALUES ('press militar');
--- Obtener el ID del ejercicio 'press militar' insertado
-SET @id_press_militar = LAST_INSERT_ID();
+-- Insertar series para el ejercicio 'sentadillas'
+INSERT INTO series (peso, repes) VALUES
+(20, 20);
+-- Obtener el ID de la serie insertada para 'sentadillas'
+SET @id_serie_sentadillas_1 = LAST_INSERT_ID();
+
+INSERT INTO series (peso, repes) VALUES
+(40, 1);
+-- Obtener el ID de la serie insertada para 'sentadillas'
+SET @id_serie_sentadillas_2 = LAST_INSERT_ID();
 
 -- Insertar series para el ejercicio 'press banca'
 INSERT INTO series (peso, repes) VALUES
@@ -33,30 +42,20 @@ INSERT INTO series (peso, repes) VALUES
 -- Obtener el ID de la serie insertada para 'press banca'
 SET @id_serie_press_banca_2 = LAST_INSERT_ID();
 
--- Insertar series para el ejercicio 'press militar'
-INSERT INTO series (peso, repes) VALUES
-(20, 20);
--- Obtener el ID de la serie insertada para 'press militar'
-SET @id_serie_press_militar_1 = LAST_INSERT_ID();
+-- Insertar rutina 'rutina1' asociada al usuario 'damago'
+INSERT INTO rutinas (id_usuario, nombre, fecha) VALUES ((SELECT id FROM usuarios WHERE nombre = 'damago'), 'rutina1', CURDATE());
+-- Obtener el ID de la rutina 'rutina1' insertada
+SET @id_rutina_rutina1 = LAST_INSERT_ID();
 
-INSERT INTO series (peso, repes) VALUES
-(40, 1);
--- Obtener el ID de la serie insertada para 'press militar'
-SET @id_serie_press_militar_2 = LAST_INSERT_ID();
-
--- Insertar rutina 'pechito' asociada al usuario 'damago'
-INSERT INTO rutinas (id_usuario, nombre, fecha) VALUES ((SELECT id FROM usuarios WHERE nombre = 'damago'), 'pechito', CURDATE());
--- Obtener el ID de la rutina 'pechito' insertada
-SET @id_rutina_pechito = LAST_INSERT_ID();
 
 -- Insertar relaciones entre ejercicios y series en la tabla rutinas_ejercicios_series
 
+-- Para el ejercicio 'sentadillas'
+INSERT INTO rutinas_ejercicios_series (id_rutina, id_ejercicio, id_serie) VALUES
+(@id_rutina_rutina1, @id_sentadillas, @id_serie_sentadillas_1),
+(@id_rutina_rutina1, @id_sentadillas, @id_serie_sentadillas_2);
+
 -- Para el ejercicio 'press banca'
 INSERT INTO rutinas_ejercicios_series (id_rutina, id_ejercicio, id_serie) VALUES
-(@id_rutina_pechito, @id_press_banca, @id_serie_press_banca_1),
-(@id_rutina_pechito, @id_press_banca, @id_serie_press_banca_2);
-
--- Para el ejercicio 'press militar'
-INSERT INTO rutinas_ejercicios_series (id_rutina, id_ejercicio, id_serie) VALUES
-(@id_rutina_pechito, @id_press_militar, @id_serie_press_militar_1),
-(@id_rutina_pechito, @id_press_militar, @id_serie_press_militar_2);
+(@id_rutina_rutina1, @id_press_banca, @id_serie_press_banca_1),
+(@id_rutina_rutina1, @id_press_banca, @id_serie_press_banca_2);
