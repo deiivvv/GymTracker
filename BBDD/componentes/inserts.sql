9e058371b526610ab73e337b84c2e2fae48f2259
Daniel Marin Gomez <dmarin@ccoo.es>
2024-05-21 08:30:01 +0200

    hechas rutinas pretederminadas y corregido archivo principal gymtracker.sql y inserts.sql

--- BBDD/componentes/inserts.sql
+++ BBDD/componentes/inserts.sql
@@ -1,5 +1,82 @@
 USE gymtracker;
 
+-- Usuario para rutinas pretederminadas
+
+insert into usuarios(id, nombre, contrasena, rol) values (-1, 'usuario_rutinas', 'usuario_rutinas', 'bloqueado');
+SET @id_usuario = -1;
+
+-- Rutina1:Pecho y Tríceps
+
+insert into rutinas(id_usuario, nombre, fecha) values(@id_usuario, "Pecho y Trícepes", "2004-01-16");
+SET @id_rutina_1 = LAST_INSERT_ID();
+
+insert into ejercicios(id, nombre) values
+(2, "press banca"),
+(5, "fondos en paralelas"),
+(14, "extensiones de triceps en polea alta"),
+(33, "flexiones diamante"),
+(37, "press de banca con mancuernas"),
+(43, "pulldown con polea alta");
+
+insert into series(peso, repes) values(0, 0);
+SET @id_serie_1 = LAST_INSERT_ID();
+
+insert into rutinas_ejercicios_series(id_rutina, id_ejercicio, id_serie) values
+(@id_rutina_1, 2, @id_serie_1),
+(@id_rutina_1, 5, @id_serie_1),
+(@id_rutina_1, 14, @id_serie_1),
+(@id_rutina_1, 33, @id_serie_1),
+(@id_rutina_1, 37, @id_serie_1),
+(@id_rutina_1, 43, @id_serie_1);
+
+-- Rutina2:Espalda y Bíceps
+
+insert into rutinas(id_usuario, nombre, fecha) values(@id_usuario, "Espalda y Bíceps", "1995-11-28");
+SET @id_rutina_2 = LAST_INSERT_ID();
+
+insert into ejercicios(id, nombre) values
+(3, "dominadas"),
+(6, "remo con barra"),
+(8, "curl de biceps con barra"),
+(24, "pajaros"),
+(38, "face pull"),
+(44, "remo con mancuerna");
+
+insert into series(peso, repes) values(0, 0);
+SET @id_serie_2 = LAST_INSERT_ID();
+
+insert into rutinas_ejercicios_series(id_rutina, id_ejercicio, id_serie) values
+(@id_rutina_2, 3, @id_serie_2),
+(@id_rutina_2, 6, @id_serie_2),
+(@id_rutina_2, 8, @id_serie_2),
+(@id_rutina_2, 24, @id_serie_2),
+(@id_rutina_2, 38, @id_serie_2),
+(@id_rutina_2, 44, @id_serie_2);
+
+-- Rutina3:Piernas y Glúteos:
+
+insert into rutinas(id_usuario, nombre, fecha) values(@id_usuario, "Espalda y Bíceps", "2004-11-5");
+SET @id_rutina_3 = LAST_INSERT_ID();
+
+insert into ejercicios(id, nombre) values
+(1, "sentadillas"),
+(4, "peso muerto"),
+(10, "zancadas"),
+(16, "hip thrust"),
+(49, "elevaciones de gemelos sentado"),
+(13, "prensa de piernas");
+
+insert into series(peso, repes) values(0, 0);
+SET @id_serie_3 = LAST_INSERT_ID();
+
+insert into rutinas_ejercicios_series(id_rutina, id_ejercicio, id_serie) values
+(@id_rutina_3, 1, @id_serie_3),
+(@id_rutina_3, 4, @id_serie_3),
+(@id_rutina_3, 10, @id_serie_3),
+(@id_rutina_3, 16, @id_serie_3),
+(@id_rutina_3, 49, @id_serie_3),
+(@id_rutina_3, 13, @id_serie_3);
+
 -- Insertar usuarios ficticios y sus perfiles
 INSERT INTO usuarios (nombre, contrasena, rol) VALUES ('damago', 'damago', 'usuario');
 INSERT INTO perfil (id_usuario, genero, edad, altura, peso) VALUES (LAST_INSERT_ID(), 'Masculino', 25, 180, 75);
@@ -10,15 +87,12 @@ INSERT INTO perfil (id_usuario, genero, edad, altura, peso) VALUES (LAST_INSERT_
 INSERT INTO usuarios (nombre, contrasena, rol) VALUES ('david', 'david', 'bloqueado');
 INSERT INTO perfil (id_usuario, genero, edad, altura, peso) VALUES (LAST_INSERT_ID(), 'Masculino', 25, 180, 75);
 
--- Insertar ejercicio 'sentadillas'
-INSERT INTO ejercicios (nombre) VALUES ('sentadillas');
--- Obtener el ID del ejercicio 'sentadillas' insertado
-SET @id_sentadillas = LAST_INSERT_ID();
 
--- Insertar ejercicio 'press banca'
-INSERT INTO ejercicios (nombre) VALUES ('press de banca');
--- Obtener el ID del ejercicio 'press banca' insertado
-SET @id_press_banca = LAST_INSERT_ID();
+-- Obtener el ID del ejercicio 'sentadillas' 
+SET @id_sentadillas = 1;
+
+-- Obtener el ID del ejercicio 'press banca' 
+SET @id_press_banca = 2;
 
 -- Insertar series para el ejercicio 'sentadillas'
 INSERT INTO series (peso, repes) VALUES
--- BBDD/gymtracker.sql
+++ BBDD/gymtracker.sql
@@ -1,6 +1,10 @@
 DROP DATABASE IF EXISTS gymtracker;
 CREATE DATABASE gymtracker;
 
+DROP USER IF EXISTS 'dad'@'localhost';
+CREATE USER 'dad'@'localhost' IDENTIFIED BY 'padre';
+GRANT ALL PRIVILEGES ON gymtracker.* TO 'dad'@'localhost';
+
 USE gymtracker;
 
 CREATE TABLE usuarios (
@@ -59,12 +63,15 @@ CREATE TABLE rutinas_ejercicios_series (
 
 USE gymtracker;
 
-insert into usuarios(id, nombre, contrasena, rol) values (-1, 'usuario_rutinas', 'usuario_rutinas', 'bloqueado');
+-- Usuario para rutinas pretederminadas
 
+insert into usuarios(id, nombre, contrasena, rol) values (-1, 'usuario_rutinas', 'usuario_rutinas', 'bloqueado');
 SET @id_usuario = -1;
 
-insert into rutinas(id_usuario, nombre, fecha) values(@id_usuario, "Pecha y Trícepes", "2004-01-16");
-SET @id_rutina = LAST_INSERT_ID();
+-- Rutina1:Pecho y Tríceps
+
+insert into rutinas(id_usuario, nombre, fecha) values(@id_usuario, "Pecho y Trícepes", "2004-01-16");
+SET @id_rutina_1 = LAST_INSERT_ID();
 
 insert into ejercicios(id, nombre) values
 (2, "press banca"),
@@ -75,66 +82,63 @@ insert into ejercicios(id, nombre) values
 (43, "pulldown con polea alta");
 
 insert into series(peso, repes) values(0, 0);
-SET @id_serie_press_banca_1 = LAST_INSERT_ID();
-insert into series(peso, repes) values(0, 0);
-SET @id_serie_press_banca_2 = LAST_INSERT_ID();
-insert into series(peso, repes) values(0, 0);
-SET @id_serie_press_banca_3 = LAST_INSERT_ID();
-
-insert into series(peso, repes) values(0, 0);
-SET @id_serie_fondos_paralelas_1 = LAST_INSERT_ID();
-insert into series(peso, repes) values(0, 0);
-SET @id_serie_fondos_paralelas_2 = LAST_INSERT_ID();
-insert into series(peso, repes) values(0, 0);
-SET @id_serie_fondos_paralelas_3 = LAST_INSERT_ID();
-
-insert into series(peso, repes) values(0, 0);
-SET @id_serie_extension_triceps_1 = LAST_INSERT_ID();
-insert into series(peso, repes) values(0, 0);
-SET @id_serie_extension_triceps_2 = LAST_INSERT_ID();
-insert into series(peso, repes) values(0, 0);
-SET @id_serie_extension_triceps_3 = LAST_INSERT_ID();
-
-insert into series(peso, repes) values(0, 0);
-SET @id_serie_flexiones_diamante_1 = LAST_INSERT_ID();
-insert into series(peso, repes) values(0, 0);
-SET @id_serie_flexiones_diamante_2 = LAST_INSERT_ID();
-insert into series(peso, repes) values(0, 0);
-SET @id_serie_flexiones_diamante_3 = LAST_INSERT_ID();
-
-insert into series(peso, repes) values(0, 0);
-SET @id_serie_press_banca_mancuernas_1 = LAST_INSERT_ID();
-insert into series(peso, repes) values(0, 0);
-SET @id_serie_press_banca_mancuernas_2 = LAST_INSERT_ID();
-insert into series(peso, repes) values(0, 0);
-SET @id_serie_press_banca_mancuernas_3 = LAST_INSERT_ID();
-
-insert into series(peso, repes) values(0, 0);
-SET @id_serie_pulldown_1 = LAST_INSERT_ID();
-insert into series(peso, repes) values(0, 0);
-SET @id_serie_pulldown_2 = LAST_INSERT_ID();
-insert into series(peso, repes) values(0, 0);
-SET @id_serie_pulldown_3 = LAST_INSERT_ID();
+SET @id_serie_1 = LAST_INSERT_ID();
 
 insert into rutinas_ejercicios_series(id_rutina, id_ejercicio, id_serie) values
-(@id_rutina, 2, @id_serie_press_banca_1),
-(@id_rutina, 2, @id_serie_press_banca_2),
-(@id_rutina, 2, @id_serie_press_banca_3),
-(@id_rutina, 5, @id_serie_fondos_paralelas_1),
-(@id_rutina, 5, @id_serie_fondos_paralelas_2),
-(@id_rutina, 5, @id_serie_fondos_paralelas_3),
-(@id_rutina, 14, @id_serie_extension_triceps_1),
-(@id_rutina, 14, @id_serie_extension_triceps_2),
-(@id_rutina, 14, @id_serie_extension_triceps_3),
-(@id_rutina, 33, @id_serie_flexiones_diamante_1),
-(@id_rutina, 33, @id_serie_flexiones_diamante_2),
-(@id_rutina, 33, @id_serie_flexiones_diamante_3),
-(@id_rutina, 37, @id_serie_press_banca_mancuernas_1),
-(@id_rutina, 37, @id_serie_press_banca_mancuernas_2),
-(@id_rutina, 37, @id_serie_press_banca_mancuernas_3),
-(@id_rutina, 43, @id_serie_pulldown_1),
-(@id_rutina, 43, @id_serie_pulldown_2),
-(@id_rutina, 43, @id_serie_pulldown_3);
+(@id_rutina_1, 2, @id_serie_1),
+(@id_rutina_1, 5, @id_serie_1),
+(@id_rutina_1, 14, @id_serie_1),
+(@id_rutina_1, 33, @id_serie_1),
+(@id_rutina_1, 37, @id_serie_1),
+(@id_rutina_1, 43, @id_serie_1);
+
+-- Rutina2:Espalda y Bíceps
+
+insert into rutinas(id_usuario, nombre, fecha) values(@id_usuario, "Espalda y Bíceps", "1995-11-28");
+SET @id_rutina_2 = LAST_INSERT_ID();
+
+insert into ejercicios(id, nombre) values
+(3, "dominadas"),
+(6, "remo con barra"),
+(8, "curl de biceps con barra"),
+(24, "pajaros"),
+(38, "face pull"),
+(44, "remo con mancuerna");
+
+insert into series(peso, repes) values(0, 0);
+SET @id_serie_2 = LAST_INSERT_ID();
+
+insert into rutinas_ejercicios_series(id_rutina, id_ejercicio, id_serie) values
+(@id_rutina_2, 3, @id_serie_2),
+(@id_rutina_2, 6, @id_serie_2),
+(@id_rutina_2, 8, @id_serie_2),
+(@id_rutina_2, 24, @id_serie_2),
+(@id_rutina_2, 38, @id_serie_2),
+(@id_rutina_2, 44, @id_serie_2);
+
+-- Rutina3:Piernas y Glúteos:
+
+insert into rutinas(id_usuario, nombre, fecha) values(@id_usuario, "Espalda y Bíceps", "2004-11-5");
+SET @id_rutina_3 = LAST_INSERT_ID();
+
+insert into ejercicios(id, nombre) values
+(1, "sentadillas"),
+(4, "peso muerto"),
+(10, "zancadas"),
+(16, "hip thrust"),
+(49, "elevaciones de gemelos sentado"),
+(13, "prensa de piernas");
+
+insert into series(peso, repes) values(0, 0);
+SET @id_serie_3 = LAST_INSERT_ID();
+
+insert into rutinas_ejercicios_series(id_rutina, id_ejercicio, id_serie) values
+(@id_rutina_3, 1, @id_serie_3),
+(@id_rutina_3, 4, @id_serie_3),
+(@id_rutina_3, 10, @id_serie_3),
+(@id_rutina_3, 16, @id_serie_3),
+(@id_rutina_3, 49, @id_serie_3),
+(@id_rutina_3, 13, @id_serie_3);
 
 -- Insertar usuarios ficticios y sus perfiles
 INSERT INTO usuarios (nombre, contrasena, rol) VALUES ('damago', 'damago', 'usuario');
@@ -147,10 +151,10 @@ INSERT INTO usuarios (nombre, contrasena, rol) VALUES ('david', 'david', 'bloque
 INSERT INTO perfil (id_usuario, genero, edad, altura, peso) VALUES (LAST_INSERT_ID(), 'Masculino', 25, 180, 75);
 
 
--- Obtener el ID del ejercicio 'sentadillas' insertado
+-- Obtener el ID del ejercicio 'sentadillas' 
 SET @id_sentadillas = 1;
 
--- Obtener el ID del ejercicio 'press banca' insertado
+-- Obtener el ID del ejercicio 'press banca' 
 SET @id_press_banca = 2;
 
 -- Insertar series para el ejercicio 'sentadillas'
