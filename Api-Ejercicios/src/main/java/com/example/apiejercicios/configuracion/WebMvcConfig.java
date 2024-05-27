7a07f7eb19854ef1bbf7348cde3a57ab15d4eabd
Alvaro <alvarogy2004@gmail.com>
2024-05-27 21:26:33 +0200

    https

--- Api-Ejercicios/src/main/java/com/example/apiejercicios/configuracion/WebMvcConfig.java
+++ Api-Ejercicios/src/main/java/com/example/apiejercicios/configuracion/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permitir acceso a todos los endpoints
-                .allowedOrigins("http://localhost:8080", "http://gymtracker.duckdns.org:8080", "http://gymtracker.duckdns.org") // Permitir solicitudes desde este origen
+                .allowedOrigins("https://localhost", "http://localhost:8080", "http://gymtracker.duckdns.org:8080", "http://gymtracker.duckdns.org", "https://gymtracker.duckdns.org") // Permitir solicitudes desde este origen
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Permitir estos métodos HTTP
                 .allowCredentials(true) // Permitir credenciales (si se están usando)
                 .maxAge(3600); // Tiempo de vida del preflight request en segundos
--- BBDD/componentes/usuario.sql
+++
@@ -1,3 +0,0 @@
-DROP USER IF EXISTS 'dad'@'localhost';
-CREATE USER 'dad'@'localhost' IDENTIFIED BY 'padre';
-GRANT ALL PRIVILEGES ON gymtracker.* TO 'dad'@'localhost';
